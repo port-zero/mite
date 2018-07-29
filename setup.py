be96ec589ad6054a33f45cd122c10e7df0d292ad
hellerve <veit@veitheller.de>
2018-07-29 17:16:52 +0200

    update package description

--- setup.py
+++ setup.py
@@ -2,14 +2,18 @@
 import os
 from setuptools import setup, find_packages
 
+with open('README.md') as f:
+    long_description = f.read()
+
 setup(
     name='mite',
     author='Veit Heller',
-    version='0.0.1',
+    version='0.0.2',
     license='MIT',
     url='https://github.com/port-zero/mite',
     description='A modern Mite wrapper for Python',
-    download_url = 'https://github.com/port-zero/mite/tarball/0.0.1',
+    long_description=long_description,
+    download_url = 'https://github.com/port-zero/mite/tarball/0.0.2',
     packages=find_packages('.'),
     install_requires=[
         "requests",
